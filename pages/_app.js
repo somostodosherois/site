42d79a3a2395915afeb37dce0e7dfe37a221c2ee
Thays Lacerda <thayslcorrea3@gmail.com>
2022-12-12 19:29:53 -0300

    fix: firebase

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import '../styles/globals.css'
 import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
-import { linkResolver, repositoryName } from '../prismicio'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
+import { linkResolver, repositoryName } from '../prismicio'
 import CommonProvider from "../contexts/common";
 import ProductsProvider from "../contexts/products";
 import CartProvider from "../contexts/cart";
@@ -13,6 +15,19 @@ import { CoinsProvider } from '../contexts/coins';
 
 
 function MyApp({ Component, pageProps }) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyB2hZgNvoEUYcqqih-DY1PDsG8T8SH_n2k",
+    authDomain: "somostodosherois-sth.firebaseapp.com",
+    projectId: "somostodosherois-sth",
+    storageBucket: "somostodosherois-sth.appspot.com",
+    messagingSenderId: "1021963600406",
+    appId: "1:1021963600406:web:fc07c1ea64e5f5ab64eb37",
+    measurementId: "G-MEC1QDKNJG"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   return (
     <PrismicProvider
       linkResolver={linkResolver}
