043a7d866a9969cda29d3917ed65cce9c6e325a2
Thays Lacerda <thayslcorrea3@gmail.com>
2023-02-02 22:57:44 -0300

    feat: add new fonts

--- components/TitleSession/TitleSession.jsx
+++ components/TitleSession/TitleSession.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TitleSession = ({ title }) => (
   <div className="max-w-2xl mx-auto py-8 md:max-w-none">
-    <h2 className="text-2xl md:text-3xl text-gray-700 font-bold"> {title}</h2>
+    <h2 className="text-2xl md:text-title text-gray-700 font-cartagena"> {title}</h2>
   </div>
 )
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,25 @@ module.exports = {
     "./slices/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      title: `2.6rem;`,
+      paragraph: `1.2rem;`
+    },
     extend: {
+      colors: {
+        primary: {
+          500: '#FF6363;',
+          800: '#FF1313;',
+        }
+      },
+      fontFamily: {
+        sans: ['sans-serif'],
+        cartagena: ['Cartagena']
+      },
       backgroundImage: {
         'fundo-criancas': "url('../public/hero-relatorio.png')",
         'fundo-herois': "url('../public/background-2.jpeg')",
       }
     },
-  },
-  plugins: [],
+  }
 }
