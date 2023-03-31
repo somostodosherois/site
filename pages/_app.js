039e9497ba24d77aae3f5a3f7e7b88d10c5a34cd
Thays Lacerda <thayslcorrea3@gmail.com>
2023-03-31 17:35:42 -0300

    fix: change context coins

--- pages/_app.js
+++ pages/_app.js
@@ -4,28 +4,12 @@ import React from 'react';
 import Link from 'next/link'
 import { PrismicProvider } from '@prismicio/react'
 import { PrismicPreview } from '@prismicio/next'
-import { initializeApp } from "firebase/app";
 
 import { linkResolver, repositoryName } from '../prismicio'
-import CommonProvider from "../contexts/common";
-import ProductsProvider from "../contexts/products";
 import CheckoutProvider from "../contexts/checkout";
 import { CoinsProvider } from '../contexts/coins';
 
 function MyApp({ Component, pageProps }) {
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyB2hZgNvoEUYcqqih-DY1PDsG8T8SH_n2k",
-    authDomain: "somostodosherois-sth.firebaseapp.com",
-    projectId: "somostodosherois-sth",
-    storageBucket: "somostodosherois-sth.appspot.com",
-    messagingSenderId: "1021963600406",
-    appId: "1:1021963600406:web:fc07c1ea64e5f5ab64eb37",
-    measurementId: "G-MEC1QDKNJG"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  // const analytics = getAnalytics(app);
 
   return (
     <PrismicProvider
@@ -37,15 +21,11 @@ function MyApp({ Component, pageProps }) {
       )}
     >
       <PrismicPreview repositoryName={repositoryName}>
-        {/* <CommonProvider> */}
-          {/* <ProductsProvider> */}
-            <CoinsProvider>
-              <CheckoutProvider>
-                <Component {...pageProps} />
-              </CheckoutProvider>
-            </CoinsProvider>
-          {/* </ProductsProvider> */}
-        {/* </CommonProvider> */}
+        <CoinsProvider>
+          <CheckoutProvider>
+            <Component {...pageProps} />
+          </CheckoutProvider>
+        </CoinsProvider>
       </PrismicPreview>
     </PrismicProvider>
   )
